b63daf1b612b0d3082ca5a32ce9705bbdec4d2b3
Sidi <diaby002@umn.edu>
2020-01-07 18:24:36 -0600

    project and footer

--- asset/js/main.js
+++ asset/js/main.js
@@ -12,7 +12,43 @@
 
     });
 
+   
+
+
+    $('#side-bar a, #intro a, #style-navbar a').on('click', function(e) {
+    e.preventDefault();
+    console.log($(this).attr('href'));
+    $('html,body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+
+
+
+  	
+
+
 });
+
+  $(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+
+
+    // Do something
+
+     if(scroll!=0){
+    	var x = $('#style-navbar').offset().top;
+
+    	console.log(x)
+    	$('#style-navbar').addClass("black-nav");
+    }
+     else{
+
+     	$('#style-navbar').removeClass("black-nav");
+     }
+});
+
+  
+
+  	
 
   ScrollReveal().reveal('#jumbo', {
     distance: '150%',
@@ -20,3 +56,24 @@
     opacity: null, 
     delay: 500
 });
+
+ScrollReveal().reveal('.skill', {
+    distance: '150%',
+    origin: 'left',
+    opacity: null, 
+    delay: 500
+});
+
+  $('.skill').tilt({
+    scale: 1.2
+})
+
+   ScrollReveal().reveal('.project', {
+    distance: '5%',
+    origin: 'bottom',
+    opacity: null, 
+    delay: 500
+});
+
+
+
